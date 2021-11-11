0d62c72fae52390022291b1c84a634c321d9bc01
Tommy Chou <tommychouyc@gmail.com>
2021-11-10 17:38:59 -0800

    add gpgpusim cpp externs

--- src/intel/vulkan/gpgpusim_calls_from_mesa.h
+++ src/intel/vulkan/gpgpusim_calls_from_mesa.h
@@ -22,4 +22,21 @@ extern void gpgpusim_vkCmdTraceRaysKHR(
                       uint64_t launch_size_addr);
 
 extern void gpgpusim_setDescriptorSet(uint32_t setID, uint32_t descID, void *address, uint32_t size, VkDescriptorType type);
+
+// CPP externs
+extern void gpgpusim_addTreelets_cpp(VkAccelerationStructureKHR accelerationStructure);
+extern uint32_t gpgpusim_registerShader_cpp(char * shaderPath, uint32_t shader_type);
+
+extern void gpgpusim_vkCmdTraceRaysKHR_cpp(
+                      void *raygen_sbt,
+                      void *miss_sbt,
+                      void *hit_sbt,
+                      void *callable_sbt,
+                      bool is_indirect,
+                      uint32_t launch_width,
+                      uint32_t launch_height,
+                      uint32_t launch_depth,
+                      uint64_t launch_size_addr);
+
+extern void gpgpusim_setDescriptorSet_cpp(uint32_t setID, uint32_t descID, void *address, uint32_t size, VkDescriptorType type);
 #endif /* GPGPUSIM_CALLS_FROM_MESA_H */
